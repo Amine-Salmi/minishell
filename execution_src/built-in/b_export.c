f01362eed80a48af52c8288983e2f52ac949f6d4
AmineSalmi01 <amine.salmi.solicode@gmail.com>
2024-10-19 16:37:15 +0100

    export is work

--- execution_src/built-in/b_export.c
+++ execution_src/built-in/b_export.c
@@ -9,22 +9,24 @@ void    print_env_var(t_env *env)
         printf("declare -x %s", env->content->var);
         if (env->content->value)
             printf("=\"%s\"\n", env->content->value);
+        else
+            printf("\n");
         env = env->next;
     }
 }
 
-int check_elements(t_env *new_var)
+int check_elements(char *var)
 {
     int i;
 
-    if (!ft_isalpha(new_var->content->var[0]) && new_var->content->var[0] != '_')
-    {
+    if (!ft_isalpha(var[0]) && var[0] != '_')
         return (1);
-    }
     i = 1;
-    while (new_var->content->var[i])
+    while (var[i])
     {
-        if (!ft_isdigit(new_var->content->var[i]) && !ft_isalpha(new_var->content->var[i]) && new_var->content->var[i] != '_')
+        if (var[i] == '=')
+            return (0);
+        if (!ft_isdigit(var[i]) && !ft_isalpha(var[i]) && var[i] != '_')
             return (1);
         i++;
     }
@@ -41,8 +43,9 @@ int elemnt_exist(t_env *var, t_env *env)
     {
         if (!ft_strncmp(var->content->var, env->content->var, ft_strlen(var->content->var)))
         {
-            env->content->value = var->content->value;
-            return 1;
+            if (var->content->value)
+                env->content->value = var->content->value;
+            return (1);
         }
         env = env->next;
     }
@@ -75,9 +78,6 @@ t_env *create_new_elemnts(char *args, t_env *env)
         new_node->content->value = ft_strdup(elemnts[1]);
     else
         new_node->content->value = NULL;
-    if (check_elements(new_node))
-        return (NULL);
-    // create function free double pointer
     free(elemnts[0]);
     if (elemnts[1])
         free(elemnts[1]);
@@ -120,15 +120,18 @@ int ft_export(t_token *cmd, t_env *env)
     }
     while (cmd->arg[i])
     {
-        new_node = create_new_elemnts(cmd->arg[i], env);
-        if (!new_node)
+        
+        if (!check_elements(cmd->arg[i]))
+        {
+            new_node = create_new_elemnts(cmd->arg[i], env);\
+            if (!elemnt_exist(new_node, env))    
+                add_to_env(env, new_node);
+        }
+        else
         {
             printf("mminishell-0.1: export: `%s': a not valid identifier\n", cmd->arg[i]);
             return (1);
         }
-        if (elemnt_exist(new_node, env))
-            return 0;
-        add_to_env(env, new_node);
         i++; 
     }
     return 0;
